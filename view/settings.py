ede94fd08be6dc19dff777562d6319f987e9adae
JohnM-Kahura <Johnmungai392@gmail.com>
2022-08-19 14:10:32 +0300

    edit settings

--- view/settings.py
+++ view/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^)3giy+g6rr@fq_e+sk3ie8@vuiw3_@7dfa=36pulfw=tr3ryn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kenyan-eye.herokuapp.com','127.0.0.1']
 
